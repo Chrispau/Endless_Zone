7ab93a11f8673bf668b8b2ae605029801f841545
Ian <icowan@ucsc.edu>
2022-04-20 13:54:30 -0700

    scene keeps track of how many yards have scrolled

--- src/scenes/play.js
+++ src/scenes/play.js
@@ -12,10 +12,14 @@ class Play extends Phaser.Scene {
         this.scrollingField = this.add.tileSprite(0, 0, game.config.width, game.config.height, 'field').setOrigin(0, 0);
         this.testSprite = this.add.tileSprite(game.config.width / 2, game.config.height/2, 0, 0, 'runner').setOrigin(0.5, 1);
         this.speed = game.config.height;
+        this.centerDistance = 0;
+        this.lastYardline = 0;
     }
 
     update(time, delta) {
         // starting speed is 10yards/s, or 1 screen length
-        this.scrollingField.tilePositionY -= this.speed * delta / 1000; // normalize scroll speed to pixels per second
+        this.scrollingField.tilePositionY -= this.speed * (delta / 1000); // normalize scroll speed to pixels per second
+        this.centerDistance += (this.speed * (delta / 1000)) / 80; // total distance the screen has scrolled so far, in yards
+        
     }
 }
