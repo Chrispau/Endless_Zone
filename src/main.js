f8432d1866fe20ff68e3d303db30d72744c8089c
Ian Cowan <icowan@ucsc.edu>
2022-04-21 00:48:32 -0700

    basic polling sprite movement with arcade physics

--- src/main.js
+++ src/main.js
@@ -1,7 +1,16 @@
+// Endless Zone
+let cursors;
+
 const config = {
     type: Phaser.CANVAS,
     width: 800,
     height: 800,
+    physics: {
+        default: 'arcade',
+        arcade: {
+            debug: true,
+        }
+    },
     scene: [Play]
 }
 
--- src/scenes/play.js
+++ src/scenes/play.js
@@ -10,12 +10,34 @@ class Play extends Phaser.Scene {
 
     create() {
         this.scrollingField = this.add.tileSprite(0, 0, game.config.width, game.config.height, 'field').setOrigin(0, 0);
-        this.testSprite = this.add.tileSprite(game.config.width / 2, game.config.height/2, 0, 0, 'runner').setOrigin(0.5, 1);
-        this.speed = game.config.height;
+        this.PLAYER_VELOCITY = 400
+        this.scrollSpeed = game.config.height;
+        // create simple cursor input
+        cursors = this.input.keyboard.createCursorKeys();
+
+        // pysics sprite
+        this.player = this.physics.add.sprite(game.config.width / 2, game.config.height/2, 'runner').setOrigin(0.5, 1);
+        this.player.setCollideWorldBounds(true);
+    
     }
+
+
 
     update(time, delta) {
         // starting speed is 10yards/s, or 1 screen length
-        this.scrollingField.tilePositionY -= this.speed * delta / 1000; // normalize scroll speed to pixels per second
+        this.scrollingField.tilePositionY -= this.scrollSpeed * delta / 1000; // normalize scroll speed to pixels per second
+        this.player.setVelocity(0);
+
+        if (cursors.left.isDown) {
+            this.player.setVelocityX(-this.PLAYER_VELOCITY);
+        } else if (cursors.right.isDown) {
+            this.player.setVelocityX(this.PLAYER_VELOCITY);
+        }
+        if (cursors.up.isDown) {
+            this.player.setVelocityY(-this.PLAYER_VELOCITY);
+        } else if (cursors.down.isDown) {
+            this.player.setVelocityY(this.PLAYER_VELOCITY);
+        }
+    
     }
 }
