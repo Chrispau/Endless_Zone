82f932c634c0c694d3ee274e3b3726d027ef8f23
Ian Cowan <icowan@ucsc.edu>
2022-04-21 01:02:49 -0700

    fixed distance count issues resulting from merge

--- src/scenes/play.js
+++ src/scenes/play.js
@@ -12,6 +12,7 @@ class Play extends Phaser.Scene {
         this.scrollingField = this.add.tileSprite(0, 0, game.config.width, game.config.height, 'field').setOrigin(0, 0);
         this.PLAYER_VELOCITY = 400
         this.scrollSpeed = game.config.height;
+
         // create simple cursor input
         cursors = this.input.keyboard.createCursorKeys();
 
@@ -19,17 +20,18 @@ class Play extends Phaser.Scene {
         this.player = this.physics.add.sprite(game.config.width / 2, game.config.height/2, 'runner').setOrigin(0.5, 1);
         this.player.setCollideWorldBounds(true);
     
-        this.testSprite = this.add.tileSprite(game.config.width / 2, game.config.height/2, 0, 0, 'runner').setOrigin(0.5, 1);
-        this.speed = game.config.height;
         this.centerDistance = 0;
-        this.lastYardline = 0;
+        //this.lastYardline = 0;
     }
 
 
 
     update(time, delta) {
         // starting speed is 10yards/s, or 1 screen length
-        this.scrollingField.tilePositionY -= this.scrollSpeed * delta / 1000; // normalize scroll speed to pixels per second
+        this.scrollingField.tilePositionY -= this.scrollSpeed * (delta / 1000); // normalize scroll speed to pixels per second
+        this.centerDistance += (this.scrollSpeed * (delta / 1000)) / (this.scrollSpeed / 10); // total distance the screen has scrolled so far, in yards
+        //console.log(this.centerDistance);
+        
         this.player.setVelocity(0);
 
         if (cursors.left.isDown) {
@@ -43,8 +45,5 @@ class Play extends Phaser.Scene {
             this.player.setVelocityY(this.PLAYER_VELOCITY);
         }
     
-        this.scrollingField.tilePositionY -= this.speed * (delta / 1000); // normalize scroll speed to pixels per second
-        this.centerDistance += (this.speed * (delta / 1000)) / 80; // total distance the screen has scrolled so far, in yards
-        
     }
 }
