53e8db9b2f3a24671353d4d64cf626bd3efa8960
Ian Cowan <icowan@ucsc.edu>
2022-04-22 22:07:19 -0700

    basic defender spawner

--- src/scenes/play.js
+++ src/scenes/play.js
@@ -18,17 +18,17 @@ class Play extends Phaser.Scene {
 
         // create simple cursor input
         cursors = this.input.keyboard.createCursorKeys();
-        // extra key for debug stuff
+        // extra key for debug stuff TODO: REMOVE/DISABLE BEFORE FINAL SUBMISSION
         keyJ = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.J);
         keyR = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.R);
 
         // physics sprite
-        this.player = this.physics.add.sprite(game.config.width / 2, game.config.height/2, 'runner')
+        this.player = this.physics.add.sprite(game.config.width / 2, game.config.height / 2, 'runner')
             .setOrigin(0.5, 1)
-            .setScale(game.config.width / 800, game.config.height / 800); 
-            // scale sprite such that it is always the same relative to screen size
+            .setScale(game.config.width / 800, game.config.height / 800);
+        // scale sprite such that it is always the same relative to screen size
         this.player.setCollideWorldBounds(true);
-    
+
         this.centerDistance = 0;
         this.defenders = this.physics.add.group({
             runChildUpdate: true
@@ -48,7 +48,9 @@ class Play extends Phaser.Scene {
         let scoreConfig = {
             fontFamily: 'Stencil Std, fantasy',
             fontSize: '56px',
-            backgroundColor: '#013220',
+            //backgroundColor: '#013220',
+            strokeThickness: 4,
+            stroke: '013220',
             color: '#FFFFFF',
             align: 'right',
             padding: {
@@ -56,39 +58,52 @@ class Play extends Phaser.Scene {
                 bottom: 5,
             },
         }
-        this.p1Score = 0; 
+        this.p1Score = 0;
         this.scoreLeft = this.add.text(0, 0, 'SCORE: ' + this.p1Score, scoreConfig);
+
+        this.obstacleSpawnDelay = 3000; // initial time between obstacles appearing in ms
+        this.obstacleSpawnTimer = this.obstacleSpawnDelay;
     }
 
 
 
     update(time, delta) {
-        // starting speed is 10yards/s, or 1 screen length
-        this.scrollingField.tilePositionY -= this.scrollSpeed * (delta / 1000); // normalize scroll speed to pixels per second
-        this.centerDistance += (this.scrollSpeed * (delta / 1000)) / (this.scrollSpeed / 10); // total distance the screen has scrolled so far, in yards
-        //console.log(this.centerDistance);
-        
+
+
         this.player.setVelocity(0);
 
-        if (cursors.left.isDown) {
-            this.player.setVelocityX(-this.PLAYER_VELOCITY);
-        } else if (cursors.right.isDown) {
-            this.player.setVelocityX(this.PLAYER_VELOCITY);
-        }
-        if (cursors.up.isDown) {
-            this.player.setVelocityY(-this.PLAYER_VELOCITY);
-        } else if (cursors.down.isDown) {
-            this.player.setVelocityY(this.PLAYER_VELOCITY);
-        }
+        if (!this.gameOver) {
 
-        if (Phaser.Input.Keyboard.JustDown(keyJ)) {
-            this.spawnDefender(300);
-        }
-        
-        //score display
-        if(!this.gameOver){
+            // starting speed is 10yards/s, or 1 screen length
+            this.scrollingField.tilePositionY -= this.scrollSpeed * (delta / 1000); // normalize scroll speed to pixels per second
+            this.centerDistance += (this.scrollSpeed * (delta / 1000) / (game.config.height / 10)); // total distance the screen has scrolled so far, in yards
+            //console.log(this.centerDistance);
+
+            //obstacle spawning
+            this.obstacleSpawnTimer -= delta;
+            if (this.obstacleSpawnTimer <= 0) {
+                this.obstacleSpawnTimer = this.obstacleSpawnDelay;
+                this.spawnDefender(300);
+            }
+
+            //score display
             this.p1Score = Math.floor(this.centerDistance);
             this.scoreLeft.text = 'SCORE: ' + this.p1Score + " YARDS";
+
+            if (cursors.left.isDown) {
+                this.player.setVelocityX(-this.PLAYER_VELOCITY);
+            } else if (cursors.right.isDown) {
+                this.player.setVelocityX(this.PLAYER_VELOCITY);
+            }
+            if (cursors.up.isDown) {
+                this.player.setVelocityY(-this.PLAYER_VELOCITY);
+            } else if (cursors.down.isDown) {
+                this.player.setVelocityY(this.PLAYER_VELOCITY);
+            }
+
+            if (Phaser.Input.Keyboard.JustDown(keyJ)) {
+                this.spawnDefender(300);
+            }
         }
         //game over display
         let gameoverConfig = {
@@ -101,17 +116,17 @@ class Play extends Phaser.Scene {
                 bottom: 5,
             },
         }
- 
-       if(this.gameOver){
+
+        if (this.gameOver) {
             this.gameoverScreen = this.add.tileSprite(0, 0, game.config.width, game.config.height, 'gg').setOrigin(0, 0);
-            this.add.text(game.config.width/2, game.config.height/6, 'GAME OVER', gameoverConfig).setOrigin(0.5);
+            this.add.text(game.config.width / 2, game.config.height / 6, 'GAME OVER', gameoverConfig).setOrigin(0.5);
             gameoverConfig.fontSize = '80px';
-            this.add.text(game.config.width/2, game.config.height/2, 'SCORE: ' + this.p1Score + ' YARDS', gameoverConfig).setOrigin(0.5);
+            this.add.text(game.config.width / 2, game.config.height / 2, 'SCORE: ' + this.p1Score + ' YARDS', gameoverConfig).setOrigin(0.5);
             gameoverConfig.fontSize = '45px';
-            this.add.text(game.config.width/2, game.config.height - 100, 'Press (R) to Restart', gameoverConfig).setOrigin(0.5);
+            this.add.text(game.config.width / 2, game.config.height - 100, 'Press (R) to Restart', gameoverConfig).setOrigin(0.5);
         }
 
-        if(this.gameOver && Phaser.Input.Keyboard.JustDown(keyR)) {
+        if (this.gameOver && Phaser.Input.Keyboard.JustDown(keyR)) {
             this.scene.restart();
         }
     }
@@ -128,7 +143,7 @@ class Play extends Phaser.Scene {
             direction = -1;
         }
         let startingY = randomRange(-game.config.height / 2, game.config.height / 2);
-        this.defenders.add (new Defender (this, startingX, startingY, 'defender', 0, speed * direction ), true); //second arg must be true to add object to display list i guess
+        this.defenders.add(new Defender(this, startingX, startingY, 'defender', 0, speed * direction), true); //second arg must be true to add object to display list i guess
     }
     setGameOver() {
         this.gameOver = true;
@@ -136,7 +151,7 @@ class Play extends Phaser.Scene {
         console.log('game over');
     }
 
-    
+
 }
 
 // get a random value in the range (works for negatives)
